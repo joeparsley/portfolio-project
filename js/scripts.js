6ba9557ac8ae5164fd8a10883f91c0d6a529db8d
Joe Parsley <voodoojoenut@gmail.com>
2017-06-17 19:49:18 -0700

    add side nav to hamburger memenu with styling

--- js/scripts.js
+++ js/scripts.js
@@ -8,23 +8,9 @@
 // FRONT END LOGIC
 $(document).ready(function(){
 
-// HAMBURGER BUTTON
-  var expanded = false;
-    $('.burger').click(function() {
-      if (!expanded) {
-        $('.burger span:first-child').removeClass('toggle-top-reverse');
-        $('.burger span:last-child').removeClass('toggle-bottom-reverse');
-        $('.burger span:first-child').addClass('toggle-top');
-        $('.burger span:last-child').addClass('toggle-bottom');
-        $('.burger span:nth-child(2)').addClass('middle');
-        expanded = true;
-      } else {
-        $('.burger span:nth-child(2)').removeClass('middle');
-        $('.burger span:first-child').removeClass('toggle-top');
-        $('.burger span:last-child').removeClass('toggle-bottom');
-        $('.burger span:first-child').addClass('toggle-top-reverse');
-        $('.burger span:last-child').addClass('toggle-bottom-reverse');
-        expanded = false;
-      }
-    });
+// SIDE NAV
+    $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+        $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+        e.preventDefault();
+      });
 });
