03f944e71ba904f7c76f0b481dd4b3db1ecbc458
Joe Parsley <voodoojoenut@gmail.com>
2017-06-18 22:17:37 -0700

    quick code refactor, vender prefixed all css

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@
 
 // FRONT END LOGIC
 $(document).ready(function(){
-
 // SIDE NAV
     $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
         $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
